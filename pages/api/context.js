5f513016a5d0f8e55ae52c427a324eece05b9519
Milo Zheng <hello@milo.works>
2023-03-31 14:57:30 +1100

    chore: comments on the state of the context data

--- pages/api/context.js
+++ pages/api/context.js
@@ -1,13 +1,13 @@
 export const context = {
   client: {
     name: "nandos",
-    clickUpSpaceId: "dsahiubc"
+    clickUpSpaceId: "dsahiubc" // @TODO: Create the task in the correct client space in ClickUp 
   },
-  techStack: ["AWS", "React", "CraftCMS"],
+  techStack: ["AWS", "React", "CraftCMS"], // Not influencing the output very well at the moment.
   platforms: ["iOS", "Android", "web"],
   currentProjects: [
     {
-      clickUpId: "abcdefg",
+      clickUpId: "abcdefg", // @TODO: Create the task in the correct project in ClickUp 
       name: "Spin To Win AU"
     }
   ],
